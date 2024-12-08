0fbda000c12afaa31541fef580f5bc973a417161
Prakhar Singh Mahori <110615983+Prakhar122002@users.noreply.github.com>
2024-12-09 01:44:15 +0530

    Add files via upload

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,236 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "Testimonials",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I' M",
+    name: "Prakhar Singh Mahori",
+    post: "WEB DEVELOPER",
+    design: "UI / UX DESIGNER",
+    desc: "Heyy! Nice to See You..",
+  },
+]
+export const about = [
+  {
+    desc: "Hardworking and passionate Computer Science Engineer with strong organizational skills eager to secure entry-level position. Ready to help team achieve company goals. I am here to seek and maintain full-time position that offers professional challenges utilizing interpersonal skills, excellent time management and problem-solving skills",
+    desc1: "With my B.Tech. in Computer Science from Lovely Professional University and an eagerness to grow, I am prepared to contribute as a software developer in a dynamic environment. I am a passionate software developer with strong skills in programming, web development, and problem-solving. Proficient in C, C++, Python, JavaScript, and frameworks like ReactJS and NodeJS, I aim to build scalable and efficient solutions.",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Creative Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "Lorem Ipsum simply text of the printing and type setting industry when an unknown printing simply",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "18",
+    title: "HAPPY CLIENTS",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "11",
+    title: "PROJECTS COMPLETED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "34",
+    title: "FILES DOWNLOADED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "1446",
+    title: "LINES OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/port3.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/port6.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/image.png",
+    name: "Prakhar Mahori",
+    post: "Software Developer",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/image.png",
+    name: "Prakhar Mahori",
+    post: "Front End Developer",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitordapibus dictum.Fusce faucibus ligula scelerisque, eleifend turpis in",
+    image: "./images/testimonials/image.png",
+    name: "Prakhar Mahori",
+    post: "React Developer",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Jun 27, 2024",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b1.png",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b2.png",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jun 27, 2023",
+    author: "Dorian Gray",
+    desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "804, Sky Gardens, Sector 16-B",
+    text2: "Noida, UP, 201301",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "+91-8923995330",
+    text2: "+91-9760692030",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "psmahori2002@gmail.com",
+    text2: "smahori@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
